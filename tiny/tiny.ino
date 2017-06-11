fac35f7e19710625e9c67706b664dc37bd76419b
Gerf Derp <gerfderp@users.noreply.github.com>
2017-06-11 13:34:55 -0700

    initial add of  PWMServo code

--- tiny/tiny.ino
+++ tiny/tiny.ino
@@ -4,11 +4,28 @@
 
 // import the libraries needed
 #include <PCJoy.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
 
 const int leftLed = 4;    // pin the left LED is attached to
 const int rightLed = 5;    // pin the right LED is attached to
 #define AUDIOPIN 3
+
+//Using Analog 0 and 1 AND Digital 6 and 7
 PCJoy myJoy(A0, A1, 6, 7);
+
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+// Depending on your servo make, the pulse width min and max may vary, you 
+// want these to be as small/large as possible without hitting the hard stop
+// for max range. You'll have to tweak them as necessary to match the servos you
+// have!
+#define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX 600 // this is the 'maximum' pulse length count (out of 4096)
+
+// our servo # counter
+uint8_t servonum = 0;
 
 void setup() {
   // make the LED pins outputs
@@ -26,6 +43,11 @@ void setup() {
   digitalWrite(rightLed, HIGH);
   delay(500);
   digitalWrite(rightLed, LOW);
+
+  //Servo Shield setup
+  pwm.begin();
+  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+  yield();
 }
 
 void loop() {
@@ -34,6 +56,27 @@ void loop() {
   if (joyState.isConnected){
     if (joyState.aDown){
       fire(); 
+    }
+    //TODO: Need to figure out current position
+    int pos0 = 0;
+    
+    while(joyState.yPos == 99){
+      if (pos <= SERVOMAX){
+        pos += 1;
+        headServo.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(headMoveDelay);                       // waits 15ms for the servo to reach the position        
+        pos = headServo.read();
+      } 
+      joyState = myJoy.getState();
+    }
+    while(joyState.yPos == -99){
+      if (pos >= SERVOMIN){
+        pos -= 1;
+        headServo.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(headMoveDelay);                       // waits 15ms for the servo to reach the position
+        pos = headServo.read();
+      }
+      joyState = myJoy.getState();
     }
   }
 }
