30ca94aa266e80a2bc756a27aa57ceaa17c87f23
Tara Neier <gerfderp@users.noreply.github.com>
2017-06-28 21:16:00 -0700

    status quo with new hardware

--- tiny/tiny.ino
+++ tiny/tiny.ino
@@ -1,3 +1,5 @@
+
+
 /*
 
  */
@@ -5,13 +7,17 @@
 // import the libraries needed
 #include <WiiChuck.h>
 #include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
 
-const int leftLed = 4;    // pin the left LED is attached to
-const int rightLed = 5;    // pin the right LED is attached to
+#define leftLed 4    // pin the left LED is attached to
+#define rightLed 5    // pin the right LED is attached to
 #define AUDIOPIN 3
 Nunchuck nunchuck(SDA, SCL);
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println("Tiny waking up!");
   //setup Nunchuck
   nunchuck.begin();
 
@@ -20,21 +26,23 @@ void setup() {
   nunchuck.addMap(new Nunchuck::buttonZ(7,200,128,10)); // Servo pin, max servo value, zero center value, min servo value 
   nunchuck.addMap(new Nunchuck::buttonC(8,200,128,10)); // Servo pin, max servo value, zero center value, min servo value 
 
-  // make the LED pins outputs
-  pinMode(leftLed, OUTPUT);
-  pinMode(rightLed, OUTPUT);
+  pwm.begin();
+  pwm.setPWMFreq(60);  // This is the maximum PWM frequency
+ 
   //make Audiopin output
   pinMode(AUDIOPIN, OUTPUT);
   // Set the pin high as the default state
   digitalWrite(AUDIOPIN, HIGH); 
   
   // cycle LEDs
-  digitalWrite(leftLed, HIGH);
+  pwm.setPWM(leftLed, 4096, 0);
   delay(500);
-  digitalWrite(leftLed, LOW);
-  digitalWrite(rightLed, HIGH);
+  pwm.setPWM(leftLed, 0, 0);
+  
+  pwm.setPWM(rightLed, 0, 0);
   delay(500);
-  digitalWrite(rightLed, LOW);
+  pwm.setPWM(rightLed, 4096, 0);
+  Serial.println("Tiny setup finish!");
 }
 
 void loop() {
@@ -50,18 +58,17 @@ void loop() {
 void fire() {     
   // change the status LEDs
   Serial.println("firing started");
-  digitalWrite(leftLed, HIGH);
+  pwm.setPWM(leftLed, 4096, 0);
   // bring the pin low to begin the activation
   digitalWrite(AUDIOPIN, LOW); 
   delay(300);
-  digitalWrite(leftLed, LOW);
+  pwm.setPWM(leftLed, 0, 0);
 
-  digitalWrite(rightLed, HIGH);
+  pwm.setPWM(rightLed, 0, 0);
   delay(200);
-  digitalWrite(rightLed, LOW);
+  pwm.setPWM(rightLed, 4096, 0);
   // bring the pin high again to end the activation
   digitalWrite(AUDIOPIN, HIGH); 
-  digitalWrite(rightLed, LOW);
 
 }
 
